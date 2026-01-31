c75a9cfb5121e3676ea79aadf5a831dce9ffd48b
root <root@v2202503261627325109.bestsrv.de>
2026-01-31 19:36:58 +0100

    Fix broken astro config from sed command

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-	site: 'https://wiki.veloxos.org/en',
+	site: 'https://wiki.veloxos.org',
 	integrations: [
 		starlight({
 			title: 'VeloxOS Documentation',
@@ -12,23 +12,27 @@ export default defineConfig({
 					label: 'English',
 					lang: 'en',
 				},
-                                de: {
-                                        label: 'German',
-                                        lang: 'de',
-                                },
+				de: {
+					label: 'German',
+					lang: 'de',
+				},
 			},
 			favicon: '/favicon.svg',
 			logo: {
 				src: './src/assets/logo.png',
 			},
+			customCss: ['./src/styles/custom.css'],
+			social: [
+				{ icon: 'github', href: 'https://github.com/hrskully' }
+			],
 			sidebar: [
 				{
 					label: 'Guides',
+					autogenerate: { directory: 'guides' },
+				},
 				{
 					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-					autogenerate: { directory: 'guides' },
+					autogenerate: { directory: 'reference' }
 				},
 			],
 		}),
